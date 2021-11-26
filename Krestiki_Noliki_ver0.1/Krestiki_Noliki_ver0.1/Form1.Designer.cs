3c6ba5950edf3a262328831b7b857dd8988d2105
Кюдяков Тимур < @corp.college.ru>
2021-11-26 14:19:54 +0900

    Krestiki_Noliki

---
+++ Krestiki_Noliki_ver0.1/Krestiki_Noliki_ver0.1/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace Krestiki_Noliki_ver0._1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.buttonPlay_Click = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(633, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "label1";
+            // 
+            // buttonPlay_Click
+            // 
+            this.buttonPlay_Click.Location = new System.Drawing.Point(636, 169);
+            this.buttonPlay_Click.Name = "buttonPlay_Click";
+            this.buttonPlay_Click.Size = new System.Drawing.Size(75, 23);
+            this.buttonPlay_Click.TabIndex = 10;
+            this.buttonPlay_Click.Text = "Начать";
+            this.buttonPlay_Click.UseVisualStyleBackColor = true;
+            this.buttonPlay_Click.Click += new System.EventHandler(this.button10_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(922, 659);
+            this.Controls.Add(this.buttonPlay_Click);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button buttonPlay_Click;
+    }
+}
+
